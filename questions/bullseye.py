e5e909e14ff59ea392892b63dea8966dc2706718
arie0703 <[ari0703sophia@gmail.com]>
2021-06-01 20:22:38 +0900

    binary searchを使ったbullseyeのプログラム

--- questions/bullseye.py
+++ questions/bullseye.py
@@ -3,24 +3,52 @@
 import math
 
 def calculate(r, t):
-    pi = math.pi
-    paint = t * pi # 1mlで1piセンチ平方メートルカバーできる。 tmlで描けるリングの面積を表す変数
-    required_inks = 0 #黒リングのトータル面積
-    n = 0 #ループ回数・リングをかける最大回数
+    # pi = math.pi
+    # paint = t * pi # 1mlで1piセンチ平方メートルカバーできる。 tmlで描けるリングの面積を表す変数
+    # required_inks = 0 #黒リングのトータル面積
+    # n = 0 #ループ回数・リングをかける最大回数
 
-    while paint > required_inks:
-        #黒リングの半径はループ回数が一回増えるごとに2cmずつ増える
-        r += 2 * n
+    # while paint > required_inks:
+    #     #黒リングの半径はループ回数が一回増えるごとに2cmずつ増える
+    #     r += 2 * n
 
-        #ring = (r + 1) ** 2 * pi - r ** 2 * pi
-        ring = (2 * r + 1) * pi
-        required_inks += ring
+    #     #ring = (r + 1) ** 2 * pi - r ** 2 * pi
+    #     ring = (2 * r + 1) * pi
+    #     required_inks += ring
 
-        if required_inks > paint:
-            break
-        n += 1
+    #     if required_inks > paint:
+    #         break
+    #     n += 1
 
-    print(n)
+    # print(n)
+
+    #  m個の円を作るために、2 * (m ** 2) + (2 * r - 1) * mのペンキが必要
+    m = (1 - 2 * r + math.sqrt((2 * r - 1) ** 2 + 8 * t)) // 4
+    print(m)
+    # この関数だと正確な数値が出ない
+
+def is_possible (m, r, t):
+    # 円をm本作れるならばTrue、作れないならFalseを返す
+    if 2 * (m ** 2) + (2 * r  - 1) * m <= t:
+        return True
+    else: 
+        return False
+
+def calculate_by_binary_search(lower, upper, r, t):
+    #r, tに加え、mの下界・上界を与える。
+    while upper - lower > 1:
+        middle = (upper + lower) // 2
+        if is_possible(middle, r, t) == True:
+            lower = middle
+        else:
+            upper = middle
+
+    if is_possible(upper, r, t) == True:
+        return upper
+    else:
+        return lower
+
+
 
 input_file = open('bullseye.txt')
 
@@ -28,7 +56,7 @@ for i in range(int(input_file.readline())):
     output = input_file.readline().split(' ')
     r = int(output[0])
     t = int(output[1])
-    calculate(r,t)
+    print(calculate_by_binary_search(0, 1000000000000000000, r,t))
 
 
 
