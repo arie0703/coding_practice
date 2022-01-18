020a8f43153c583be4971a09ab5f4fdeb32a1898
arie0703 <[ari0703sophia@gmail.com]>
2022-01-19 07:44:04 +0900

    jumpgame canjump

--- questions/leetcode/jumpgame.py
+++ questions/leetcode/jumpgame.py
@@ -6,6 +6,11 @@ def jump(nums):
         jump = nums[now]
         farthest = 0
         next_idx = 0
+
+        if nums[now] == 0:
+            print("Last Indexに辿り着くのは不可能")
+            return -1
+
         for i in range(now + 1, now + jump + 1):
             #現在地の数字から飛べる範囲で、任意の場所のindexと数字を足した数字が一番大きいところを次の移動開始位置とする
             print("range", now + 1, now + jump + 1)
@@ -26,9 +31,31 @@ def jump(nums):
 
     
 
-print(jump([1,1,1,1]))
-print(jump([2,3,0,1,4]))
-print(jump([9,7,9,4,8,1,6,1,5,6,2,1,7,9,0]))
+# print(jump([1,1,1,1]))
+# print(jump([2,3,0,1,4]))
+# print(jump([9,7,9,4,8,1,6,1,5,6,2,1,7,9,0]))
+
+# Greedy
+def canJump(nums):
+    j = 0
+
+    # j = 一個前のインデックスから飛べる最大のインデックスを表す
+    for i, n in enumerate(nums):
+        print(i,n,j)
+
+        # jが現在のインデックスを上回っている場合、進行不可となる
+        if j < i: 
+            return False
+        
+        j = max(j, i+n)
+    return True
+
+
+# print(canJump([1,1,1,1]))
+# print(canJump([2,3,0,1,4]))
+# print(canJump([9,7,9,4,8,1,6,1,5,6,2,1,7,9,0]))
+print(canJump([3,2,1,0,4]))
+
 
         
         
